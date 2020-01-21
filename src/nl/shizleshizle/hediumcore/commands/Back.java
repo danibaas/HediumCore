42b3954682f2b32fece7b0b0008f966f2360a384
Wouter de Bruijn <git@electrogamez.nl>
2020-01-21 15:14:38 +0100

    Added commandMaster and updated Back to use it.

--- src/nl/shizleshizle/hediumcore/commands/Back.java
+++ src/nl/shizleshizle/hediumcore/commands/Back.java
@@ -1,14 +1,36 @@
 package nl.shizleshizle.hediumcore.commands;
 
+import nl.shizleshizle.hediumcore.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
-public class Back implements CommandExecutor {
+import java.util.List;
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        return false;
+public class Back {
+    private static class Executor implements CommandExecutor {
+
+        @Override
+        public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+            Player player = (Player) commandSender;
+
+            return true;
+        }
+    }
+
+    private static class Tabcompleter implements TabCompleter {
+
+        @Override
+        public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+            return null;
+        }
+    }
+
+    public static void load() {
+        Main.p.getCommand("Back").setExecutor(new Executor());
+        Main.p.getCommand("Back").setTabCompleter(new Tabcompleter());
     }
 }
---
+++ src/nl/shizleshizle/hediumcore/utils/CommandMaster.java
@@ -0,0 +1,9 @@
+package nl.shizleshizle.hediumcore.utils;
+
+import nl.shizleshizle.hediumcore.commands.Back;
+
+public class CommandMaster {
+    void loadCommands() {
+        Back.load();
+    }
+}
